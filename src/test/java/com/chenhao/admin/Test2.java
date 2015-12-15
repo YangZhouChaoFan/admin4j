9df7597107fab83ff3d835bc010192b3d8db4c7f
chenhao <chenhao1000@live.cn>
2015-12-15 14:34:43 +0800

    chenhao

--- src/test/java/com/chenhao/admin/Test2.java
+++ src/test/java/com/chenhao/admin/Test2.java
@@ -1,12 +1,9 @@
 package com.chenhao.admin;
 
-import com.alibaba.fastjson.JSON;
-import com.chenhao.admin.dao.RoleMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
