150a029968648d68daf0f102f048ac2fc091dcbe
chenhao <chenhao1000@live.cn>
2015-12-29 19:33:40 +0800

    chenhao

--- src/main/webapp/modules/chart/chartCtrl.js
+++ src/main/webapp/modules/chart/chartCtrl.js
@@ -7,34 +7,38 @@
 
 'use strict';
 define(['angular'], function (angular) {
-    return function ($scope) {
+    return function ($scope, $timeout) {
+        $timeout(function(){
 
-        //曲线图
-        $scope.labelsLine = ["January", "February", "March", "April", "May", "June", "July"];
-        $scope.seriesLine = ['Series A', 'Series B'];
-        $scope.dataLine = [
-            [65, 59, 80, 81, 56, 55, 40],
-            [28, 48, 40, 19, 86, 27, 90]
-        ];
-        $scope.onClickLine = function (points, evt) {
-            console.log(points, evt);
-        };
+            //曲线图
+            $scope.labelsLine = ["January", "February", "March", "April", "May", "June", "July"];
+            $scope.seriesLine = ['Series A', 'Series B'];
+            $scope.dataLine = [
+                [65, 59, 80, 81, 56, 55, 40],
+                [28, 48, 40, 19, 86, 27, 90]
+            ];
+            $scope.onClickLine = function (points, evt) {
+                console.log(points, evt);
+            };
 
-        //条形图
-        $scope.labelsBar = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-        $scope.seriesBar = ['Series A', 'Series B'];
+            //条形图
+            $scope.labelsBar = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+            $scope.seriesBar = ['Series A', 'Series B'];
 
-        $scope.dataBar = [
-            [65, 59, 80, 81, 56, 55, 40],
-            [28, 48, 40, 19, 86, 27, 90]
-        ];
+            $scope.dataBar = [
+                [65, 59, 80, 81, 56, 55, 40],
+                [28, 48, 40, 19, 86, 27, 90]
+            ];
 
-        //饼状图
-        $scope.labelsPie = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-        $scope.dataPie = [300, 500, 100];
+            //饼状图
+            $scope.labelsPie = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
+            $scope.dataPie = [300, 500, 100];
 
-        //雷达图
-        $scope.labelsArea = ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"];
-        $scope.dataArea = [300, 500, 100, 40, 120];
+            //雷达图
+            $scope.labelsArea = ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"];
+            $scope.dataArea = [300, 500, 100, 40, 120];
+
+        }, 100);
+
     };
 });
