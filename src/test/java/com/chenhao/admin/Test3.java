0e909dc5258f823ff53090c4a1935a4c366171f2
chenhao <chenhao1000@live.cn>
2016-01-04 17:39:48 +0800

    chenhao

---
+++ src/test/java/com/chenhao/admin/Test3.java
@@ -0,0 +1,30 @@
+package com.chenhao.admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring.xml"})
+
+public class Test3 {
+
+    private static final Logger logger = LoggerFactory.getLogger(Test3.class);
+
+    @Value("${jdbc.url}")
+    private String url;
+
+
+    @Test
+    public void test() {
+        logger.info(url);
+    }
+
+}
