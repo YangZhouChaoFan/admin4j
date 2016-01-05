b3b49ce289a6a32d4b1a9f2d8a4320b6b93220e4
chenhao <chenhao1000@live.cn>
2016-01-05 13:11:50 +0800

    增加配置类，方便通过environment读取properties

--- src/main/java/com/chenhao/admin/config/AppConfig.java
+++ src/main/java/com/chenhao/admin/config/AppConfig.java
@@ -1,23 +1,10 @@
 package com.chenhao.admin.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.env.Environment;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.converter.ByteArrayHttpMessageConverter;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.GsonHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.DefaultResponseErrorHandler;
-import org.springframework.web.client.RestTemplate;
-
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @PropertySources({
